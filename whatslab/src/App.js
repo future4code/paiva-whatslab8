d0aac02fe3bcb6e3becbfdc6d8751b428f7f14e3
alvesgarrido <matheus.agarrido@gmail.com>
2021-05-07 16:19:24 -0300

    Estilizações

--- whatslab/src/App.js
+++ whatslab/src/App.js
@@ -16,6 +16,19 @@ const MessengerApp = styled.div`
 
 `
 
+const JanelaMenssagens = styled.div`
+    flex: 1 1 0%;
+    display: flex;
+    flex-direction: column;
+    -webkit-box-pack: end;
+    justify-content: flex-end;
+    padding: 20px;
+`
+
+const InputsMenssagens = styled.div`
+    display: grid;
+    grid-template-columns: 2fr 7fr 1fr;
+`
 
 export default class App extends React.Component {
   
@@ -25,14 +38,14 @@ export default class App extends React.Component {
   return (
     <MainContainer>
       <MessengerApp>
-        <div>
+        <JanelaMenssagens>
           <p>Aqui vão as mensagens</p>
-        </div>
-        <div>
+        </JanelaMenssagens>
+        <InputsMenssagens>
           <input type="text" placeholder="Usuário"></input>
           <input type="text" placeholder="Mensagem"></input>
           <button>Enviar</button>
-        </div>
+        </InputsMenssagens>
       </MessengerApp>
     </MainContainer>
   );
