6c2d3a93385b5d21a34b4e5ed09e05b95ccb635a
lausmpc <lauranunesc.@gmail.com>
2021-05-08 15:02:38 -0300

    Adicionadas funções dos inputs. Alterei o nome de alguns componentes pra deixar um padrão um pouco melhor

--- whatslab/src/App.js
+++ whatslab/src/App.js
@@ -10,43 +10,78 @@ const MessengerApp = styled.div`
   max-width: 600px;
   height: 100vh;
   border: 1px solid black;
+  box-sizing: border-box;
   flex: 1 1 0%;
   display: flex;
   flex-direction: column;
 
 `
-const JanelaMenssagens = styled.div`
+const WindowMessages = styled.div`
     flex: 1 1 0%;
     display: flex;
-    flex-direction: column;
+    flex-direction: column-reverse;
     -webkit-box-pack: end;
     justify-content: flex-end;
     padding: 20px;
 `
 
-const InputsMenssagens = styled.div`
+const InputMessages = styled.div`
     display: grid;
     grid-template-columns: 2fr 7fr 1fr;
 `
 
-export default class App extends React.Component {
+class App extends React.Component {
+
+  state = {
+    messages: [],
+    userValue: "",
+    messageValue: ""
+  }
+
+  onChangeUserValue = (event) => {
+    this.setState({userValue: event.target.value})
+  }
+
+  onChangeMessageValue = (event) => {
+    this.setState({messageValue: event.target.value})
+  }
+
+  sendMessage = () => {
+    const newMessage = {
+      user: this.state.userValue,
+      text: this.state.messageValue
+
+    }
+    const newMessageArray = [newMessage, ...this.state.messages]
+
+    this.setState({messages: newMessageArray, messageValue: ""})
+  }
+
+
   
   render() {
-
-     
+      
   return (
     <MainContainer>
       <MessengerApp>
-        <JanelaMenssagens>
-          <p>Aqui vão as mensagens</p>
-        </JanelaMenssagens>
-        <InputsMenssagens>
-          <input type="text" placeholder="Usuário"></input>
-          <input type="text" placeholder="Mensagem"></input>
-          <button>Enviar</button>
-        </InputsMenssagens>
+        <WindowMessages>
+          {this.state.messages.map((message) => {
+            return <p><strong>{message.user}</strong>: {message.text}</p>
+          })}
+        </WindowMessages>
+        <InputMessages>
+          <input onChange={this.onChangeUserValue} 
+            value={this.state.user} 
+            placeholder="Usuário"></input>
+          <input onChange={this.onChangeMessageValue} 
+            value={this.state.message}
+            placeholder="Mensagem"></input>
+          <button onClick={this.sendMessage}>Enviar</button>
+        </InputMessages>
       </MessengerApp>
     </MainContainer>
   );
+  }
 }
-}
+
+export default App;
